293017a7ee845819b6fe8af7012caaa2b26fc2a4
Himanshusinghofficial <hs882677@gmail.com>
2020-08-11 00:33:46 +0530

    change index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+router.get('/some-server-route', (req, res) => {
+    res.redirect('/assets/some-local-file');
+});
